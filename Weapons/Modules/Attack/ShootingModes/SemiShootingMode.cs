853fbe23033f7f9eae755697d92019bc63306b7f
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-07-01 21:26:17 +0200

    feat: create shooting modes
    create semi shooting mode

---
+++ Weapons/Modules/Attack/ShootingModes/SemiShootingMode.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox;
+
+namespace EasyWeapons.Weapons.Modules.Attack.ShootingModes;
+
+public class SemiShootingMode : ShootingMode
+{
+    public override bool ShouldAttack()
+    {
+        return false;
+    }
+
+    public override bool ShouldAttack(InputButton inputButton)
+    {
+        return Input.Pressed(inputButton);
+    }
+
+    public override void OnShot()
+    {
+        base.OnShot();
+        IsShooting = false;
+    }
+}
---
+++ Weapons/Modules/Attack/ShootingModes/ShootingMode.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+using System;
+
+namespace EasyWeapons.Weapons.Modules.Attack.ShootingModes;
+
+public abstract partial class ShootingMode : BaseNetworkable
+{
+    [Net, Predicted, Local]
+    public bool IsShooting { get; protected set; } = false;
+
+    public abstract bool ShouldAttack(InputButton inputButton);
+    public abstract bool ShouldAttack();
+
+    public virtual void OnShot()
+    {
+        IsShooting = true;
+    }
+
+    public virtual void OnFail()
+    {
+        IsShooting = false;
+    }
+}
