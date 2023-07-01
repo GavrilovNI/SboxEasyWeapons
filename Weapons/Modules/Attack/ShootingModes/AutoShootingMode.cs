22a4101e395e3e65b3109b7d848291508fa3c0af
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-07-01 22:44:41 +0200

    feat: create AutoShootingMode

---
+++ Weapons/Modules/Attack/ShootingModes/AutoShootingMode.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+
+namespace EasyWeapons.Weapons.Modules.Attack.ShootingModes;
+
+public class AutoShootingMode : ShootingMode
+{
+    public override bool ShouldAttack()
+    {
+        return false;
+    }
+
+    public override bool ShouldAttack(string inputAction)
+    {
+        bool shouldAttack = Input.Pressed(inputAction) || (IsShooting && Input.Down(inputAction));
+        if(shouldAttack == false)
+            IsShooting = false;
+        return shouldAttack;
+    }
+}
