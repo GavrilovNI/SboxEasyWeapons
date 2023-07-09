3de0ec2c0d30463321e21f48d2716162678dd953
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-07-10 01:50:49 +0200

    feat: create entity bullets spawner

---
+++ Bullets/IBullet.cs
@@ -0,0 +1,8 @@
+﻿using Sandbox;
+
+namespace EasyWeapons.Bullets;
+
+public interface IBullet
+{
+    void Initialize(Ray ray, DamageInfo? damageInfo);
+}
---
+++ Bullets/Spawners/EntityBulletSpawner.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+
+namespace EasyWeapons.Bullets.Spawners;
+
+public partial class EntityBulletSpawner : BulletSpawner
+{
+    [Net, Local]
+    public string BulletEntityName { get; set; } = null!;
+
+    public override void Spawn(Ray ray, DamageInfo? damageInfo)
+    {
+        if(Game.IsServer == false)
+            return;
+
+        var entityType = TypeLibrary.GetType<Entity>(BulletEntityName)?.TargetType;
+
+        if(entityType == null)
+        {
+            Log.Error($"Bullet entity '{BulletEntityName}' not found");
+            return;
+        }
+
+        if(!TypeLibrary.HasAttribute<SpawnableAttribute>(entityType))
+        {
+            Log.Error($"Bullet entity '{BulletEntityName}' is not spawnable");
+            return;
+        }
+
+        var entityBullet = TypeLibrary.Create<Entity>(entityType);
+
+        if(entityBullet is not IBullet bullet)
+        {
+            Log.Error($"Bullet entity '{BulletEntityName}' doesn't implement {nameof(IBullet)}");
+            entityBullet.Delete();
+            return;
+        }
+
+        using(Prediction.Off())
+            bullet.Initialize(ray, damageInfo);
+    }
+}
