3ebc2b42ec525b87d7e6bcb7d0079bbda9d79c37
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-07-10 01:50:06 +0200

    feat: create TimedDestroyer

---
+++ Entities/Components/TimedDestroyer.cs
@@ -0,0 +1,37 @@
+﻿using Sandbox;
+using System;
+
+namespace EasyWeapons.Entities.Components;
+
+public partial class TimedDestroyer : EntityComponent
+{
+    [Net, Local]
+    public TimeSpan TimeToDestroy { get; set; } = TimeSpan.Zero;
+
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        Event.Register(this);
+    }
+
+
+    protected override void OnDeactivate()
+    {
+        Event.Unregister(this);
+        base.OnDeactivate();
+    }
+
+    [GameEvent.Tick]
+    protected virtual void Tick()
+    {
+        TimeToDestroy = TimeToDestroy.Subtract(TimeSpan.FromSeconds(Time.Delta));
+
+        if(TimeToDestroy.TotalSeconds <= 0)
+            DeleteEntity();
+    }
+
+    protected virtual void DeleteEntity()
+    {
+        Entity.Delete();
+    }
+}
