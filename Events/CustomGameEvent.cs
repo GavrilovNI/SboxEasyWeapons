cfb56eaa5a39d53ec7ec56e9867057c48e44747d
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-08-20 17:21:54 +0200

    feat: add BuildInput post event

---
+++ Events/CustomGameEvent.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+
+namespace EasyWeapons.Events;
+
+public class CustomGameEvent
+{
+    public class Client
+    {
+        public class BuildInput
+        {
+            public const string Post = "buildinput.post";
+
+            public class PostAttribute : EventAttribute
+            {
+                public PostAttribute() : base(Post) { }
+            }
+        }
+    }
+}
